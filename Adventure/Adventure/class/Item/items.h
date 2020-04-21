39c2cd847ce884fa66fd7966a2bb7da903ea4208
elie <edarouj@hotmail.com>
2020-04-21 18:14:09 +0200

    item.h update

--- Adventure/Adventure/class/Item/items.h
+++ Adventure/Adventure/class/Item/items.h
@@ -10,16 +10,16 @@ enum ePotions
 {
     POTION_RES,
     POTION_HEAL,
-    
+    POTION_ARMOR
 };
 
 
 class Weapons
 {
-
+    private: int physical_damage; int magic_damage;
 };
 
 class Potions
 {
-
+    private: int resistance; int life; int armor;
 };
